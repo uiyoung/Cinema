3814e62fc2ef0e516db4ae235507fcbd06d300da
UiyoungSeo <suy2suy@gmail.com>
2017-05-20 02:21:56 +0900

    update

--- src/models/SeatBean.java
+++ src/models/SeatBean.java
@@ -1,40 +1,43 @@
 package models;
 
 public class SeatBean {
-	private String seatNo;
+	private String theater_no;
 	private String date;
 	private String time;
+	private String seat_no;
 	private String state;
 
 	// Getters and Setters
-	public String getSeatNo() {
-		return seatNo;
+	public String getTheater_no() {
+		return theater_no;
 	}
 
-	public void setSeatNo(String seatNo) {
-		this.seatNo = seatNo;
+	public void setTheater_no(String theater_no) {
+		this.theater_no = theater_no;
 	}
-
 	public String getDate() {
 		return date;
 	}
-
 	public void setDate(String date) {
 		this.date = date;
 	}
-
 	public String getTime() {
 		return time;
 	}
-
 	public void setTime(String time) {
 		this.time = time;
 	}
 
+	public String getSeat_no() {
+		return seat_no;
+	}
+
+	public void setSeat_no(String seat_no) {
+		this.seat_no = seat_no;
+	}
 	public String getState() {
 		return state;
 	}
-
 	public void setState(String state) {
 		this.state = state;
 	}

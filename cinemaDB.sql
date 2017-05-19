4a04e102e974f0263b0f09435307792afefab918
UiyoungSeo <suy2suy@gmail.com>
2017-05-20 02:20:57 +0900

    add ticket table

--- cinemaDB.sql
+++ cinemaDB.sql
@@ -86,23 +86,19 @@ INSERT INTO schedule_tb values(3,'201752','22:00',1,1);
 INSERT INTO schedule_tb values(4,'201752','19:00',1,2);
 INSERT INTO schedule_tb values(5,'201753','22:00',3,1);
 
-/*exampes*/
+/*examples*/
 /*201752에 영화를 상영하는 극장 찾기*/
 select name from theater_tb where no = (select distinct movie_no from schedule_tb where date='201752');
-
 /*201752에 미녀와야수를 상영하는 극장 번호*/
 select theater_no from schedule_tb where movie_no = (select no from movie_tb where title='美女と野獣');
-
 /*201752에 미녀와야수를 상영하는 극장 이름, 시간*/
 select t.name, s.time
 from movie_tb m left join schedule_tb s on m.no = s.movie_no right join theater_tb t on s.theater_no = t.no 
 where m.title = '美女と野獣' and s.date = '201752';
-
 /* 미녀와야수를 상영하고 있는 극장 불러오기 */
 select t.name
 from movie_tb m left join schedule_tb s on m.no = s.movie_no right join theater_tb t on s.theater_no = t.no 
 where m.title = '美女と野獣';
-
 /* 美女と野獣영화를 상영하는 tokyo 극장의 201752 일자의 상영시간을 불러오기 */
 select time
 from movie_tb m left join schedule_tb s on m.no = s.movie_no right join theater_tb t on s.theater_no = t.no 
@@ -112,14 +108,15 @@ where m.title = '美女と野獣' and t.name='tokyo' and s.date = '201752' ;
 /* Seat Table */
 CREATE TABLE `seat_tb` (
 	`theater_no` INT(11) NOT NULL,
-	`seat_no` VARCHAR(10) NOT NULL,
 	`date` VARCHAR(10) NOT NULL,
 	`time` CHAR(5) NOT NULL,
+	`seat_no` VARCHAR(10) NOT NULL,
 	`state` CHAR(3) NOT NULL DEFAULT 'n',
 	PRIMARY KEY (`theater_no`, `seat_no`),
 	INDEX `FK__schedule_tb` (`theater_no`),
 	INDEX `date` (`date`),
 	INDEX `time` (`time`),
+	INDEX `seat_no` (`seat_no`),
 	CONSTRAINT `seat_tb_ibfk_1` FOREIGN KEY (`theater_no`) REFERENCES `theater_tb` (`no`),
 	CONSTRAINT `seat_tb_ibfk_2` FOREIGN KEY (`date`) REFERENCES `schedule_tb` (`date`),
 	CONSTRAINT `seat_tb_ibfk_3` FOREIGN KEY (`time`) REFERENCES `schedule_tb` (`time`)
@@ -127,8 +124,6 @@ CREATE TABLE `seat_tb` (
 COLLATE='utf8_general_ci'
 ENGINE=InnoDB
 ;
-
-
 
 
 /* 좌석정보 삽입 */
@@ -160,7 +155,32 @@ select seat_no, date, state
 from theater_tb join seat_tb 
 where theater_tb.name="tokyo";
 
-/* TODO : 201752일자의 14:00시 tokyo극장의  좌석번호, 예매상태 불러오기  */
+/* 201752일자의 tokyo극장, 14:00시의   좌석번호, 예매상태 불러오기  */
 select seat_no, date, state
 from theater_tb join seat_tb 
 where theater_tb.name='tokyo' and date='201752' and time='14:00';
+
+
+/*ticket table*/
+CREATE TABLE `ticket_tb` (
+	`no` INT(11) NOT NULL AUTO_INCREMENT,
+	`title` VARCHAR(60) NOT NULL,
+	`theater_name` VARCHAR(20) NOT NULL,
+	`date` VARCHAR(10) NOT NULL,
+	`time` VARCHAR(10) NOT NULL,
+	`seat_no` VARCHAR(10) NOT NULL,
+	`price` INT(11) NOT NULL,
+	`user_id` VARCHAR(20) NOT NULL,
+	PRIMARY KEY (`no`)
+)
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB
+AUTO_INCREMENT=6
+;
+
+/* tokyo영화관의 201752일의 14:00시의 A01 좌석의 state를 y로 바꾸기 */
+update seat_tb as s join theater_tb as t
+on s.theater_no = t.no and t.name='tokyo' and s.date='201752' and s.time='14:00' and s.seat_no='A01'
+set s.state='y';
+
+

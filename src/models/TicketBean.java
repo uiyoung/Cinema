3ccc552bffc495268a092e3ffc55650c3cab85bb
UiyoungSeo <suy2suy@gmail.com>
2017-05-20 02:22:47 +0900

    create TicketBean

---
+++ src/models/TicketBean.java
@@ -0,0 +1,59 @@
+package models;
+
+public class TicketBean {
+	private int no;
+	private String title;
+	private String theater_name;
+	private String date;
+	private String time;
+	private int price;
+
+	// Getters and Setters
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getTheater_name() {
+		return theater_name;
+	}
+
+	public void setTheater_name(String theater_name) {
+		this.theater_name = theater_name;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getTime() {
+		return time;
+	}
+
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+}
